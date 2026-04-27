c1c9e48a0839a47b3edd108b12779df506de0cde
vsairesse <vsairesse@gmail.com>
2026-04-27 14:30:09 +0200

    init

---
+++ cdepda/s1/main.js
@@ -0,0 +1,44 @@
+const questions = [
+    {
+        id: '',
+        question: `Na figura abaixo \\(|BD|=\\sqrt{${Math.round(2 + Math.random() * 10)}}\\). Determine \\(\\alpha\\),  \\(x\\) e \\(y\\)`,
+        img: `<img src='1.png' width="40%"/>`
+    },
+    {
+        id: '',
+        question: `Nas figuras abaixo \\(|AB|=\\sqrt{${2 + Math.round(Math.random() * 5)}}\\), \\(|BC|=${3 + Math.round(Math.round(Math.random() * 8))}\\), \\(|DE|=x\\) e \\(|EF|=${3 + Math.round(Math.random() * 5)}x+4\\). Determine \\(x\\) .`,
+        img: `<img src='2.png' width="30%"/>`
+    },
+    {
+        id: '',
+        question: `Nas figuras abaixo \\(\\alpha\\) e \\(\\beta\\) medem \\(45^\\circ\\) e \\(60^\\circ\\) respectivamente, \\(|AB|=${Math.round(5 + Math.random() * 5)}\\), \\(|AC|=\\sqrt{${2 + Math.round(Math.random() * 3)}}\\). Determine \\(|BC|\\) em cada uma das figuras.`,
+        img: `<img src='3.png' width="40%"/>`
+    },
+]
+
+let qs = ''
+
+for (let i = 0; i < questions.length; i++) {
+    const q = questions[i];
+    qs += `<li><p>${q.question}</p>${q.img || ''}</li>`
+}
+
+const el = document.querySelector('ol')
+el.innerHTML = qs;
+
+MathJax.typesetPromise([el]);
+
+function generatePDF() {
+    const element = document.querySelector('body');
+    const nome_std = document.querySelector('input').value
+
+    html2pdf()
+        .from(element)
+        .save(nome_std + '-CDEPDA_S1' + '.pdf');
+}
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault()
+    document.querySelector('button').style.display = 'none'
+    generatePDF()
+})
---
+++ cdepda/s2/main.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        id: '',
+        question: `Determine as alturas das torres:`,
+        img: `<img src='duas-tores.jpg' width="30%"/>`
+    },
+    {
+        id: '',
+        question: `Na figura abaixo \\(d_1\\) e \\(d_3\\) são perpendiculares, \\(d_3\\) excede \\(d_1\\) em \\(${2 + Math.round(Math.random() * 5)}\\) unidades e \\(d_2\\) mede \\(${10 + Math.round(Math.random() * 7)}\\) m. Determine \\(d_1\\) e \\(d_3\\).`,
+        img: `<img src='tor3.jpg' width="25%"/>`
+    },
+    {
+        id: '',
+        question: `Um cabo eléctrico está preso entre dois postes de mesma altura
+        (${Math.round(Math.random() * 50) + 10} m), separados por ${Math.ceil(Math.random() * 15) + 7} m. O cabo forma um triângulo com o ponto mais
+        baixo, a ${Math.round(Math.random() * 3) + 1}/${Math.round(Math.random() * 3) + 5} do poste da esquerda e a ${Math.ceil(Math.random() * 5) + 2} m do solo. Determine o comprimento
+        do cabo.`
+    },
+    {
+        id: '',
+        question: `Dois cabos partem do topo de um poste formando ângulos de ${(Math.random() * 70).toFixed(2)}°
+        e ${(Math.random() * 70).toFixed(2)}° com o solo, em direcções opostas. Determine o ângulo que cada um
+        dos cabos faz com o poste.`
+    }
+]
+
+let qs = ''
+
+for (let i = 0; i < questions.length; i++) {
+    const q = questions[i];
+    qs += `<li><p>${q.question}</p>${q.img || ''}</li>`
+}
+
+const el = document.querySelector('ol')
+el.innerHTML = qs;
+
+MathJax.typesetPromise([el]);
+
+function generatePDF() {
+    const element = document.querySelector('body');
+    const nome_std = document.querySelector('input').value
+
+    html2pdf()
+        .from(element)
+        .save(nome_std + '-CDEPDA_S2' + '.pdf');
+}
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault()
+    document.querySelector('button').style.display = 'none'
+    generatePDF()
+})
